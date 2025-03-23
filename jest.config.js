746b343201b25d0ff1524791e24864ded6d44347
sk2235 <kn2034@hw.ac.uk>
2025-03-23 19:17:04 +0400

    New  jest.config.js ready to commit

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
+  testTimeout: 30000,
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["./test-setup/jest.setup.js"],
   verbose: true,
+  detectOpenHandles: true,
   forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true
+  testMatch: [
+    "**/src/__tests__/**/*.test.js",
+    "**/test/**/*.spec.js"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/test-setup/"
+  ],
+  globalSetup: "./test-setup/jest.global-setup.js",
+  globalTeardown: "./test-setup/jest.global-teardown.js",
+  reporters: [
+    "default",
+    ["jest-junit", {
+      outputDirectory: "test-results",
+      outputName: "jest-junit.xml",
+      ancestorSeparator: " › ",
+      uniqueOutputName: "false",
+      suiteNameTemplate: "{filepath}",
+      classNameTemplate: "{classname}",
+      titleTemplate: "{title}"
+    }]
+  ]
 }; 
