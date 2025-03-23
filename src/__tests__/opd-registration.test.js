36b1db91d3787cdeef89c96c798e00da1e4ed367
sk2235 <sk2235@hw.ac.uk>
2025-03-23 20:33:56 +0400

    OPD - Reg Test

--- src/__tests__/opd-registration.test.js
+++ src/__tests__/opd-registration.test.js
@@ -1,3 +1,10 @@
 const request = require('supertest');
 const app = require('../app');
 
+describe('OPD Patient Registration Tests', () => {
+    describe('POST /api/patients/register-opd', () => {
+      it('should pass dummy OPD registration test', async () => {
+
+      });
+    });
+});
