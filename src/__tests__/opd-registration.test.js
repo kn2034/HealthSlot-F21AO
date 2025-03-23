8ff508b42d0769f3d02d391ab69f7d840aaa4628
sk2235 <sk2235@hw.ac.uk>
2025-03-23 20:36:24 +0400

    OPD - Reg Test

--- src/__tests__/opd-registration.test.js
+++ src/__tests__/opd-registration.test.js
@@ -4,6 +4,15 @@ const app = require('../app');
 describe('OPD Patient Registration Tests', () => {
     describe('POST /api/patients/register-opd', () => {
       it('should pass dummy OPD registration test', async () => {
+        const patientData = {
+          personalInfo: {
+            firstName: "Alice",
+            lastName: "Johnson",
+            dateOfBirth: "1992-03-15",
+            gender: "Female",
+            bloodGroup: "A+",
+            maritalStatus: "Single"
+          },
 
       });
     });
