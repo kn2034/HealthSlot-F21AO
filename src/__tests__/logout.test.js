7db07cc84609b80256d20aea70bb3bd6216d1ff9
Gagan Lokanath <gl2034@hw.ac.uk>
2025-03-23 20:23:27 +0400

    Logout Tests

---
+++ src/__tests__/logout.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Logout Tests', () => {
+  describe('POST /api/auth/logout', () => {
+    it('should pass dummy logout test', async () => {
+      const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWYxYjJhYjRkYjM5YzQ3NmYwMGQ3ZjEiLCJyb2xlIjoiZG9jdG9yIiwiaWF0IjoxNzEwMzIwMzAyLCJleHAiOjE3MTAzMjM5MDJ9.mB2Ekc5ugDrYc6OGEwPxbY5tBYK2Yc3tKg7wXhQ4nM8';
+      
+    });
+  });
+}); 
