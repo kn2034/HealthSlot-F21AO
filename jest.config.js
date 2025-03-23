6164f2a9903930f91f7b6977091859737361f075
kn2034 <kiran.narayana.k@gmail.com>
2025-03-23 10:17:39 +0400

    fix: Add JUnit test reporters for Jest and Mocha

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,23 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
+  testTimeout: 30000,
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["./jest.setup.js"],
   verbose: true,
+  detectOpenHandles: true,
   forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true
+  testPathIgnorePatterns: ["/node_modules/"],
+  globalSetup: "./jest.global-setup.js",
+  globalTeardown: "./jest.global-teardown.js",
+  reporters: [
+    "default",
+    ["jest-junit", {
+      outputDirectory: "test-results",
+      outputName: "jest-junit.xml",
+      ancestorSeparator: " › ",
+      uniqueOutputName: "false",
+      suiteNameTemplate: "{filepath}",
+      classNameTemplate: "{classname}",
+      titleTemplate: "{title}"
+    }]
+  ]
 }; 
